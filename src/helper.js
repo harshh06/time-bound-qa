62c6d8a2fb26d60a0227e10c72ccc32b171010a3
hsoni3 <harsh_soni@apple.com>
2023-02-27 19:43:05 +0530

    added questions

--- src/helper.js
+++ src/helper.js
@@ -1,22 +1,22 @@
 export const questions = [
     {
-        question: "Q1",
-        options: ["a", "b", "c", "d"],
-        answer: "a",
+        question: "Q1. What country has the highest life expectancy?",
+        options: ["Hong Kong", "India", "Japan", "USA"],
+        answer: "Hong Kong",
     },
     {
-        question: "Q2",
-        options: ["a", "b", "c", "d"],
-        answer: "b",
+        question: "Q2. Which planet has the most moons? ",
+        options: ["Uranus", "Saturn", "Jupiter", "Mercury"],
+        answer: "Saturn",
     },
     {
-        question: "Q3",
-        options: ["a", "b", "c", "d"],
-        answer: "c",
+        question: "Q3. How many bones do we have in an ear?",
+        options: ["1", "2", "3", "4"],
+        answer: "3",
     },
     {
-        question: "Q4",
-        options: ["a", "b", "c", "d"],
-        answer: "a",
+        question: "Q4. In what country is the Chernobyl nuclear plant located?",
+        options: ["Ukraine", "Adygea", "Buryatia", "Bashkortostan"],
+        answer: "Ukraine",
     },
 ];
